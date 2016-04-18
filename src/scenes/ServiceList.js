fb2b0523109681f8b3ecc55db3f66a6b0208c540
Jakub Kondrat <jkondrat@soldevelo.com>
2016-04-18 20:14:55 +0200

    Adding basic services screen

--- src/scenes/ServiceList.js
+++ src/scenes/ServiceList.js
@@ -1,4 +1,31 @@
-import React, { Component, PropTypes, View, Text } from 'react-native';
+import React, {
+    Component,
+    PropTypes,
+    View,
+    Text,
+    ListView,
+    StyleSheet,
+    TouchableHighlight
+} from 'react-native';
+
+const REQUEST_URL = 'http://api.refugee.info/v1/services/search/?format=json';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column'
+    },
+    listViewContainer: {
+        flex: 1,
+        flexDirection: 'column'
+    },
+    buttonContainer: {
+        flex: 1,
+        flexDirection: 'column',
+        padding: 15,
+        backgroundColor: '#EEE'
+    }
+});
 
 export default class ServiceList extends Component {
 
@@ -6,14 +33,67 @@ export default class ServiceList extends Component {
         navigator: PropTypes.object.isRequired
     };
 
-    render() {
-        const { navigator } = this.context;
-
+    static renderLoadingView() {
         return (
             <View>
-                <Text>Service List</Text>
+                <Text>Loading services...</Text>
             </View>
         );
     }
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            dataSource: new ListView.DataSource({
+                rowHasChanged: (row1, row2) => row1 !== row2
+            }),
+            loaded: false
+        };
+    }
+
+    componentDidMount() {
+        this.fetchData();
+    }
+
+    fetchData() {
+        fetch(REQUEST_URL)
+            .then((response) => response.json())
+            .then((responseData) => {
+                this.setState({
+                    dataSource: this.state.dataSource.cloneWithRows(responseData),
+                    loaded: true
+                });
+            })
+            .done();
+    }
+
+    renderRow(row) {
+        return (
+            <TouchableHighlight
+                onPress={() => {}}
+                style={styles.buttonContainer}
+                underlayColor="white"
+            >
+                <Text>{row.name}</Text>
+            </TouchableHighlight>
+        );
+    }
+
+    render() {
+        const { navigator } = this.context;
+
+        if (!this.state.loaded) {
+            return ServiceList.renderLoadingView();
+        } else {
+            return (
+              <View style={styles.container}>
+                  <ListView
+                      dataSource={this.state.dataSource}
+                      renderRow={this.renderRow}
+                      style={styles.listViewContainer}
+                  />
+              </View>
+            )
+        }
+    }
 }
