8bb9364adfd2ce2b5d34858ac543b55f7c6ca390
Kamil Krampa <kkrampa@soldevelo.com>
2016-04-21 17:04:31 +0200

    Added initial version of landing page

---
+++ src/components/RegionDrillDown.js
@@ -0,0 +1,178 @@
+import React, { AsyncStorage, Component, StyleSheet, View, Picker, Text, TouchableHighlight } from 'react-native';
+import ApiClient from '../utils/ApiClient';
+
+export default class RegionDrillDown extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            countries: [],
+            cities: [],
+            countryId: null,
+            cityId: null,
+            loading: true
+        };
+        this.apiClient = new ApiClient();
+    }
+
+    componentDidMount() {
+        this._loadInitialState();
+    }
+
+    async _loadInitialState() {
+        const countries = await this.apiClient.getRootLocations();
+        this._setCountries(countries);
+        let region = JSON.parse(await AsyncStorage.getItem('region'));
+        const changes = {loading: false};
+        if (region) {
+            changes['countryId'] = region.countryId;
+            await this._onCountryChange(region.countryId).done();
+            if (region.countryId != region.id) {
+                changes['cityId'] = region.id;
+            }
+        }
+        this.setState(changes);
+
+    }
+
+    _setCountries(countries) {
+        let defaultOption = [
+            {id: '', name: 'Select Country'}
+        ];
+
+        this.setState({
+            countries: defaultOption.concat(countries)
+        });
+    }
+
+    getCountries() {
+        return this.state.countries.map(
+            (country, idx) => <Picker.Item key={idx} label={country.name} value={country.id}/>
+        )
+    }
+
+    getCities() {
+        return this.state.cities.map(
+            (city, idx) => <Picker.Item key={idx} label={city.name} value={city.id}/>
+        )
+    }
+
+    countryPicker() {
+        return (
+            <Picker onValueChange={this._onCountryChange.bind(this)}
+                selectedValue={this.state.countryId}
+            >
+                {this.getCountries()}
+            </Picker>
+        );
+    }
+
+    cityPicker() {
+        if (!this.state.countryId) {
+            return;
+        }
+        return (
+            <Picker selectedValue={this.state.cityId} onValueChange={(value) => this.setState({cityId: value})}>
+                {this.getCities()}
+            </Picker>
+        );
+    }
+
+    async _onCountryChange(value) {
+        try {
+            this.setState({countryId: value, cities: [], cityId: null, loading: true});
+            let regions = await this.apiClient.getRegions(value);
+
+            let cities = [];
+            const children = await this.apiClient.getCities(value);
+            cities = cities.concat(children);
+            for (let region of regions) {
+                cities = cities.concat(await this.apiClient.getCities(region.id));
+            }
+
+            const defaultOption = [
+                {id: '', name: '-----'}
+            ];
+
+            this.setState({cities: defaultOption.concat(cities), loading: false});
+        } catch (e) {
+            this.setState({loading: false});
+        }
+
+    }
+
+    loading() {
+        if (this.state.loading) {
+            return (<Text style={styles.loading}>Loading...</Text>);
+        }
+    }
+
+    _getRegionById(regionId) {
+        const allRegions = this.state.cities.concat(this.state.countries);
+        return allRegions.find((x) => x.id === regionId);
+    }
+
+    _selectedId() {
+        let selectedId = this.state.countryId;
+        if (this.state.cityId) {
+            selectedId = this.state.cityId;
+        } else if (this.state.regionId) {
+            selectedId = this.state.regionId;
+        }
+        return selectedId;
+    }
+
+    onPress() {
+        const region = this._getRegionById(this._selectedId());
+        region.countryId = this.state.countryId;
+        this.props.onPress(region);
+    }
+
+    selectButton() {
+        const region = this._getRegionById(this._selectedId());
+
+        if (!region || this.state.loading) {
+            return;
+        }
+
+        return (
+            <TouchableHighlight
+                onPress={this.onPress.bind(this)}
+                style={styles.button}
+                underlayColor="white"
+            >
+                    <Text style={styles.buttonText}>Select {region.name}</Text>
+            </TouchableHighlight>
+        )
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                {this.countryPicker()}
+                {this.cityPicker()}
+                {this.loading()}
+                {this.selectButton()}
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 5,
+        flex: 1
+    },
+    loading: {
+        justifyContent: 'center',
+        alignSelf: 'center'
+    },
+    button: {
+        backgroundColor: '#EEE'
+    },
+    buttonText: {
+        alignSelf: 'center',
+        fontSize: 15,
+        color: 'black'
+    }
+});
--- src/scenes/GeneralInformation.js
+++ src/scenes/GeneralInformation.js
@@ -1,4 +1,4 @@
-import React, { Component, PropTypes, View, Text } from 'react-native';
+import React, { Component, PropTypes, View, Text, AsyncStorage, Alert } from 'react-native';
 
 export default class GeneralInformation extends Component {
 
@@ -6,12 +6,29 @@ export default class GeneralInformation extends Component {
         navigator: PropTypes.object.isRequired
     };
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            region: ''
+        };
+    }
+
+    componentDidMount() {
+        this._loadInitialState().done();
+    }
+
+    async _loadInitialState() {
+        let region = await AsyncStorage.getItem('region');
+        this.setState({region: JSON.parse(region)});
+    }
+
     render() {
         const { navigator } = this.context;
 
         return (
             <View>
                 <Text>General Info</Text>
+                <Text>{this.state.region.name}</Text>
             </View>
         );
     }
