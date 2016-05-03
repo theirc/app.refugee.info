08d6334ec9e82d7bfeb276d0244dc73914900380
Kamil Krampa <kkrampa@soldevelo.com>
2016-05-03 14:18:56 +0200

    Components refactoring

--- src/scenes/GeneralInformationDetails.js
+++ src/scenes/GeneralInformationDetails.js
@@ -3,10 +3,18 @@ import {View, WebView} from 'react-native';
 
 
 export default class GeneralInformationDetails extends Component {
+
+    static propTypes = {
+        section: React.PropTypes.string.isRequired
+    };
+
     render() {
         return (
             <View style={{height: 600}}>
-                <WebView style={{marginBottom: 50}} source={{html: this.props.section}}/>
+                <WebView
+                    source={{html: this.props.section}}
+                    style={{marginBottom: 50}}
+                />
             </View>
         )
     }
