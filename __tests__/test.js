815e2403250fae8efc463447ac65a55471567d3d
Kamil Krampa <kkrampa@soldevelo.com>
2016-05-05 15:06:11 +0200

    Added unit tests

--- __tests__/test.js
+++
@@ -1,7 +0,0 @@
-describe('test', () => {
-  it('adds 1 + 1 to equal 2', () => {
-      const expected = 2;
-      expect(1 + 1).toBe(expected);
-    }
-  );
-});
---
+++ src/__mocks__/react-native.js
@@ -0,0 +1,49 @@
+/**
+ * # __mockes__/react-native.js
+ *
+ * This class stubs out the React-Native classes with React classes
+ */
+'use strict';
+/**
+ * ## Imports
+ *
+ * ReactNative is actually React
+ */
+import React, { Component } from 'react';
+const ReactNative = React;
+
+/**
+ * ## These need additional mocking
+ *
+ * ReactNative is actually React
+ */
+ReactNative.StyleSheet = {
+    create: function create(styles) {
+        return styles;
+    }
+};
+class View extends Component {
+    render() { return false; }
+}
+class PixelRatio extends Component {
+    static get() { return 1; }
+}
+/**
+ * ## Stubs
+ *
+ * Simple replacements for testing
+ */
+ReactNative.View = View;
+ReactNative.ScrollView = View;
+ReactNative.Text = View;
+ReactNative.TouchableOpacity = View;
+ReactNative.TouchableHighlight = View;
+ReactNative.TouchableWithoutFeedback = View;
+ReactNative.ToolbarAndroid = View;
+ReactNative.Image = View;
+ReactNative.PixelRatio = PixelRatio;
+ReactNative.Picker = View;
+ReactNative.NativeModules= {};
+
+ReactNative.Platform = {};
+module.exports = ReactNative;
