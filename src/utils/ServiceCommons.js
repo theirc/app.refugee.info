ca2a1b09d924b2377dd277561d8223e1d8bc91c2
Jakub Kondrat <jkondrat@soldevelo.com>
2016-05-05 16:42:53 +0200

    Show service icons by type

--- src/utils/ServiceCommons.js
+++ src/utils/ServiceCommons.js
@@ -9,7 +9,25 @@ import { default as Icon } from 'react-native-vector-icons/FontAwesome';
 
 import Messages from '../constants/Messages';
 
-const styles = StyleSheet.create();
+const styles = StyleSheet.create({
+    icon: {
+        width: 32,
+        height: 32
+    },
+    container: {
+        flex: 1,
+        flexDirection: 'row'
+    },
+    iconContainer: {
+        width: 32,
+        marginRight: 10,
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    detailsContainer: {
+        flexDirection: 'column'
+    }
+});
 
 export default class ServiceCommons {
 
@@ -24,14 +42,18 @@ export default class ServiceCommons {
             />
           ));
         return (
-            <View>
-                <Image
-                    source={{uri: serviceType.icon_url}}
-                    style={styles.icon}
-                />
-                <Text>{service.name}</Text>
-                <Text>{Messages.RATING}: {stars}</Text>
-                <Text>{locationName}</Text>
+            <View style={styles.container}>
+                <View style={styles.iconContainer}>
+                    <Image
+                        source={{uri: service.icon_url}}
+                        style={styles.icon}
+                    />
+                </View>
+                <View style={styles.detailsContainer}>
+                    <Text>{service.name}</Text>
+                    <Text>{Messages.RATING}: {stars}</Text>
+                    <Text>{locationName}</Text>
+                </View>
             </View>
         );
     }
