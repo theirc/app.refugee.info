79de56dba802fe85f18a39e2d0fa556155b489b5
Rey Rodrigues <rodriguesreynaldo@gmail.com>
2016-07-09 16:17:14 -0400

    Actual scene

---
+++ src/scenes/Notifications.js
@@ -0,0 +1,81 @@
+import React, {Component, PropTypes} from 'react';
+import {
+    View,
+    Text,
+    ListView,
+    RefreshControl,
+    StyleSheet,
+    TouchableHighlight,
+    AsyncStorage,
+    TextInput,
+    Image,
+    WebView
+} from 'react-native';
+import I18n from '../constants/Messages';
+import ApiClient from '../utils/ApiClient';
+import ServiceCommons from '../utils/ServiceCommons';
+import MapButton from '../components/MapButton';
+import {OfflineView, SearchBar, SearchFilterButton} from '../components';
+import {connect} from 'react-redux';
+import Icon from 'react-native-vector-icons/Ionicons';
+import InfiniteScrollView from 'react-native-infinite-scroll-view';
+import {Regions, Services} from '../data';
+import styles, {themes, getUnderlayColor, generateTextStyles, getRowOrdering, getAlignItems} from '../styles';
+import {wrapHtmlContent} from '../utils/htmlUtils'
+
+var _ = require('underscore');
+
+
+export class Notifications extends Component {
+    render() {
+        if (!this.props.region) {
+            return <View />
+        }
+        const {language, theme} = this.props;
+
+        let backgroundColor = theme == 'light' ? themes.light.backgroundColor : themes.dark.backgroundColor;
+        let webViewStyle = { backgroundColor: backgroundColor, opacity: 1 };
+        let html = this.props.region.metadata.banners.map((b, i) => `<div class="banner">${b}</div>`).join('<br />');
+        let source = {
+            html: wrapHtmlContent(html, language, '', theme)
+        };
+
+        console.log(source.html);
+
+        return <View style={styles.container}>
+            <WebView ref={(v) => this.webView = v}
+                style={webViewStyle}
+                source={source}
+                />
+        </View>;
+    }
+}
+
+const htmlStyles = StyleSheet.create({
+    div: {
+        padding: 0,
+        margin: 0,
+    },
+    p: {
+        padding: 0,
+        margin: 0,
+    },
+    strong: {
+        padding: 0,
+        margin: 0,
+    },
+    br: {
+        padding: 0,
+        margin: 0,
+    },
+});
+
+function mapStateToProps(state) {
+    return {
+        theme: state.theme.theme,
+        language: state.language,
+        region: state.region,
+    }
+}
+
+export default connect(mapStateToProps)(Notifications);
