589ce92282c1072a454fc566a57689546ab4a867
Rey Rodrigues <rodriguesreynaldo@gmail.com>
2016-07-26 20:51:42 -0400

    Fixing icon on Android

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -2,7 +2,9 @@ import styles, {
     getIconComponent,
     getIconName,
 } from '../styles';
-import React, {Component, PropTypes} from 'react'
+import HumanitarianIcon from './HumanitarianIcon';
+import React, {Component, PropTypes, } from 'react';
+import {StyleSheet, Platform} from 'react-native';
 
 export default class Icon extends Component {
     static propTypes = {
@@ -12,6 +14,16 @@ export default class Icon extends Component {
         let props = {...this.props };
         const IconComponent = getIconComponent(props.name);
         props.name = getIconName(props.name);
+        let style = StyleSheet.flatten([props.style]);
+        props.style = style;
+
+        if (Platform.OS != 'ios') {
+            if (HumanitarianIcon == IconComponent) {
+                if (style.fontSize) {
+                    style.fontSize -= 4;
+                }
+            }
+        }
 
         return (<IconComponent {...props}></IconComponent>)
     }
